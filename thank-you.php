04e6a5fef519516e9cd5c4bb2fbc2749fa54232f
leison <leisonkennedy@gmail.com>
2014-12-17 00:30:30 +0300

    Added a success page

---
+++ thank-you.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Switch to DStv:switch to the package that’s right for you</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="HandheldFriendly" content="True">
+        <meta name="MobileOptimized" content="320">
+        <!--[if lt IE 9]>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <!--[if IE 7]>     <html class="ie7"> <![endif]-->  
+        <!--[if IE 8]>     <html class="ie8"> <![endif]-->  
+        <!--[if IE 9]>     <html class="ie9"> <![endif]-->  
+        <!--[if !IE]><!--> <html>             <!--<![endif]-->
+        <link rel="stylesheet" href="css/main.css">
+        <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+        <script type="text/javascript" src="js/flipclock.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+    </head>
+    <body>
+        <div class="campaign-logo"> <img src="images/campaign-logo.png" alt=""> </div>
+
+        <div class="dstv-logo"><img src="images/dstv-logo.png" alt=""></div>
+
+        <div class="body-bg"></div>
+        
+        <div class="content-main">
+            <div class="right-wing">
+                <div class="display-table">
+                    <div class="vertical-align">
+                        <div class="fixer">
+                            <div class="wrap">
+                                <div class="success">Thank you for your interest. <br> We will contact you shortly.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div class="left-wing">
+                <div class="display-table">
+                    <div class="vertical-align">
+                        <div class="banner">
+                            <img src="images/dstv-hd-decoder.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="copyright-bar">
+                    <div class="copy-text">&copy; 2014 MultiChoice (PTY) LTD. All rights reserved.</div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="footer">
+            &copy; 2014 MultiChoice (PTY) LTD. All rights reserved.
+        </div>
+
+      
+
+    </body>
+</html>
