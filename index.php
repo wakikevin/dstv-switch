bf52dd7d62965cdfe78453394505f1fa46f6506f
leison <leisonkennedy@gmail.com>
2014-12-16 19:35:00 +0300

    Added the initial files.

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Switch to DStv:switch to the package that’s right for you</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="HandheldFriendly" content="True">
+        <meta name="MobileOptimized" content="320">
+        <!--[if lt IE 9]>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <!--[if IE 7]>     <html class="ie7"> <![endif]-->  
+        <!--[if IE 8]>     <html class="ie8"> <![endif]-->  
+        <!--[if IE 9]>     <html class="ie9"> <![endif]-->  
+        <!--[if !IE]><!--> <html>             <!--<![endif]-->
+        <link rel="stylesheet" href="css/main.css">
+        <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+        <script type="text/javascript" src="js/flipclock.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+    </head>
+    <body>
+        <div class="campaign-logo"> <img src="images/campaign-logo.png" alt=""> </div>
+
+        <div class="dstv-logo"><img src="images/dstv-logo.png" alt=""></div>
+        
+        <div class="body-bg"></div>
+        
+        <div class="content-main">
+            <div class="right-wing">
+                <div class="display-table">
+                    <div class="vertical-align">
+                        <div class="fixer">
+                            <div class="wrap">
+                                    <div class="counter-box">
+                                    <h3>The countdown to digital is on:</h3>
+                                    <div class="clock"></div>
+                                    <div class="message"></div>
+                                    <div class="order-form"> 
+                                    <h4>To get this special online offer fill in the form below:</h4>
+                                   <form action="">
+                                       <input type="text" placeholder="Name">
+                                       <input type="text" placeholder="Phone number">
+                                       <input type="text" placeholder="Email">
+                                       <div class="selectdiv">  
+                                            <select class="selectboxdiv">
+                                              <option>Which town is closest to you</option>
+                                              <option>Nairobi</option>
+                                              <option>Kisumu</option>
+                                              <option>Mombasa</option>
+                                              <option>Narok</option>
+                                            </select>
+                                            <div class="out">Nairobi</div>
+                                        </div>
+                                        <input type="button" value="get a call back">
+                                   </form>
+                               </div>
+                                </div>
+                                <div class="disclaimer">By submiting this form you consent to DStv using your information as entered above.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div class="left-wing">
+                <div class="display-table">
+                    <div class="vertical-align">
+                        <div class="banner">
+                            <img src="images/dstv-hd-decoder.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="copyright-bar">
+                    <div class="copy-text">&copy; 2014 MultiChoice (PTY) LTD. All rights reserved.</div>
+                </div>
+            </div>
+
+        </div>
+
+      
+
+    </body>
+</html>
